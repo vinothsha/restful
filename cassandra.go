a9b79aba8bfa5fb64a8f0a6400b8facc886e90a8
vinothsha <74014926+vinothsha@users.noreply.github.com>
2022-03-16 14:34:53 +0530

    Update cassandra.go

--- cassandra.go
+++ cassandra.go
@@ -11,7 +11,7 @@ var Session *gocql.Session
 func init() {
 	var err error
 	cluster := gocql.NewCluster("127.0.0.1")
-	cluster.Keyspace = "restfulapi"
+	cluster.Keyspace = "restful"
 	cluster.Consistency = gocql.LocalOne
 	Session, err = cluster.CreateSession()
 	if err != nil {
